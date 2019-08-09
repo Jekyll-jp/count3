29080a4e4ea2afd2de6ab9373759965cb5b99e4d
student <ygpzmc28@gmail.com>
2019-08-09 11:00:57 +0900

    20190809

--- src/count3/game.java
+++ src/count3/game.java
@@ -1,12 +1,12 @@
 /**
  * 数値を1回～3回入力していき、最後に最終ナンバーを入力させた方が勝者となる
  *
- * class Main >>> メインロジック
- * ArrayList<String> players() >>> プレイヤーの名前を決めるロジック
- * ArrayList<String> playerRandom(ArrayList<String> list)  >>> 先攻後攻を決めるロジック
- *
- *
- *
+ **void main(String[] args) throws IOException >>> メインロジックというか設定
+ **ArrayList<String> players() >>> プレイヤーの名前を決めるロジック
+ **ArrayList<String> playerRandom(ArrayList<String> list)  >>> 先攻後攻を決めるロジック
+ **int  playerchange(int player) >>> 対戦時プレイヤーを変更するロジック
+ **void start(ArrayList number, ArrayList player) >>> 対戦ロジック
+ **void space(int space) >>> コンソールを少しでも見やすくする為にspaceを入力するロジック
  */
 
 package count3;
@@ -19,35 +19,24 @@ import java.util.Scanner;
 public class game {
 
 	public static void main(String[] args) throws IOException {
-		int max;
-		Scanner sc = new Scanner(System.in);
-		//1-1：コンソール上に最初の文字を表示
-		System.out.println("到達上限を入力して下さい");
 
-		//1-2：入力した上限数値をStringで保管しコンソール上に表示
+		Scanner sc = new Scanner(System.in);
+		//コンソール上に最初の文字を表示
+		System.out.println("到達上限数値を半角で入力して下さい");
+
+		//入力した上限数値をStringで保管しコンソール上に表示
 		String MAX = sc.next();
-		for (int a = 0; a < 3; a++) {
-			System.out.println("");
-		} //3行空白
+		space(3);
 		System.out.println("目標到達数値は:【" + MAX + "】です");
 
-		//1-3：上限数値をint型に置き換えた変数max
-		max = Integer.parseInt(MAX);
-
-		// 1-4：リスト型のnumbersに1～maxまでをStringに変換して入れる処理
+		//リスト型のnumbersに1～MAXまでをStringに変換して入れる処理
 		ArrayList<String> numbers = new ArrayList<String>();
-		for (int n = 1; n <= max; n++) {
+		for (int n = 1; n <= Integer.parseInt(MAX); n++) {
 			numbers.add(String.valueOf(n));
 		}
 
 		//プレイヤー入力(仮決定のプレイヤーのリストを作成)
-		ArrayList<String> players = new ArrayList<String>();
-		try {
-			players = players();
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		ArrayList<String> players = players();
 
 		//先攻後攻決め(プレイヤー名も確定)
 		ArrayList<String> player = playerRandom(players);
@@ -74,9 +63,7 @@ public class game {
 
 		while (flag == true) {
 			//flagがtrueの時反復する
-			for (int a = 0; a < 3; a++) {
-				System.out.println("");
-			} //3行空白
+			space(3);
 			System.out.println("「" + players.get(pl) + "」さん" + (count + 1) + "回目の数値を入力してください");
 			String alerts05 = "次の数値は【" + numbers.get(0) + "】です。    残り入力回数は【" + (3 - count) + "】回です。\r\n";
 			alerts = new StringBuilder();
@@ -89,66 +76,59 @@ public class game {
 
 			String num = sc.next();
 
-			//flagをfalseにする条件(ゲーム終了条件)
-			//①	数値入力が上限数値の場合
-			//②	ENDを入力した際
+			//入力したものが半角数値でない場合("A"は除く)
 			if (!num.equals(numbers.get(0)) && !num.equals("A")) {
 				System.out.println("入力した数値が不正です");
 				continue;
 			}
 
-			//エラー系 PASS無効化
-			if (num.equals("A") && count == 0) {
-				System.out.println("1回目の入力なのでパスはできませんでした");
-				continue;
-			}
+			//終了条件(最後まで)
 			if (num.equals(numbers.get(numbers.size() - 1)) || num.equals("END")) {
 				flag = false;
-				if (pl == 1) {
-					count = 0; //初期に戻す
-					pl = 0;
-				} else if (pl == 0) {
-					count = 0; //初期に戻す
-					pl = 1;
-				}
+				count = 0;
+				pl = playerchange(pl);
 				break;
 			}
-			if(num.equals("A") && count !=0) {
-				if (pl == 1) {
-					count = 0; //初期に戻す
-					pl = 0;
-				} else if (pl == 0) {
-					count = 0; //初期に戻す
-					pl = 1;
+
+			//相手へパスするとき
+			if (num.equals("A")) {
+				if (count == 0) { //パスできないのにパスした場合
+					System.out.println("1回目の入力なのでパスはできませんでした");
+				} else { //パスできる場合
+					count = 0;
+					pl = playerchange(pl);
 				}
 				continue;
 			}
+
 			//正規処理 入力数値とリストの0番目が等しいときで1回目か2回目の場合
 			//countをインクリメントして、リストから要素を除く処理
 			if (num.equals(numbers.get(0)) && count < 4) {
-				count++; //この時点でcountが3になった場合は下記にて1に戻してプレイヤー交代
+				count++;
 				numbers.remove(0);
-				if (count == 3 && pl == 1) {
-					count = 0; //初期に戻す
-					pl = 0;
-				} else if (count == 3 && pl == 0) {
-					count = 0; //初期に戻す
-					pl = 1;
+				//この時点でcountが3の場合countを1に戻してプレイヤー交代
+				if (count == 3) {
+					count = 0;
+					pl = playerchange(pl);
 				}
 				continue;
 			}
 
 		}
 		System.out.println("「" + players.get(pl) + "」さんの勝利です。");
-
 		sc.close();
 	}
 
-	//プレイヤー変更ロジック
-	public static int  playerchange(int player) {
+	public static int playerchange(int player) {
 
-
-
+		if (player == 1) {
+			player = 0;
+		} else if (player == 0) {
+			player = 1;
+		} else {
+			System.out.println("プレイヤー変更の際にエラーが発生しました。プログラムを終了します。");
+			System.exit(0);
+		}
 		return player;
 	}
 
@@ -170,25 +150,19 @@ public class game {
 		if (!nameb.equals("A")) {
 			do {
 				namea = nameb;
-				for (int a = 0; a < 3; a++) {
-					System.out.println("");
-				} //3行空白
+				space(3);
 				System.out.print("変更後のプレイヤーAの名前は" + namea + "でよろしいですか？\r\n"
 						+ "よろしければ【A】を入力しEnterを押下して下さい。");
 				nameb = sc.next();
 			} while (!nameb.equals("A"));
 		} else {
-			for (int a = 0; a < 3; a++) {
-				System.out.println("");
-			} //3行空白
+			space(3);
 			System.out.print("【" + namea + "】さん 登録ありがとうございます。\r\n"
 					+ "つづいてプレイヤーBの名前を入力して下さい。");
 		}
 		players.add(namea);
 		namea = sc.next();
-		for (int a = 0; a < 3; a++) {
-			System.out.println("");
-		} //3行空白
+		space(3);
 		System.out.print("プレイヤーBの名前は" + namea + "でよろしいですか？\r\r\n"
 				+ "よろしければ【A】を入力しEnterを押下して下さい。\r\n"
 				+ "変更の場合は新しいプレイヤーBの名前を入力して下さい。");
@@ -196,9 +170,7 @@ public class game {
 		nameb = sc.next();
 		if (!nameb.equals("A")) {
 			do {
-				for (int a = 0; a < 3; a++) {
-					System.out.println("");
-				} //3行空白
+				space(3);
 				namea = nameb;
 				System.out.print("変更後のプレイヤーAの名前は" + namea + "でよろしいですか？\r\n"
 						+ "よろしければ【A】を入力しEnterを押下して下さい。");
@@ -220,12 +192,17 @@ public class game {
 			players.add(players.get(0));
 			players.remove(0);
 		}
-		for (int a = 0; a < 3; a++) {
-			System.out.println("");
-		} //3行空白
+		space(3);
 		System.out.print("先攻後攻をランダムで決定します\r\n"
 				+ "先攻は「" + players.get(0) + "」さんに決まりました\r\n");
 
 		return players;
 	}
+
+	public static void space(int space) {
+		for (int a = 0; a < space+1; a++) {
+			System.out.println("");
+		}
+	}
+
 }
